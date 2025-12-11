33dc622590ffabc32bbf4c86783855a81d5444a5
Homepc <wisdomadeyemi77@gmail.com>
2025-12-11 12:23:32 +0100

    Fix database config and update settings

--- church/settings.py
+++ church/settings.py
@@ -102,12 +102,27 @@ WSGI_APPLICATION = 'church.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#         conn_max_age=600
+#     )
+# }
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#         ssl_require=True,
+#     )
+# }
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
+
 
 
 # DATABASES = {
